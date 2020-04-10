881e9921e8901cf85442a23a2fbe9652339a035e
jonguangg <jonguangg@163.com>
2020-04-10 22:55:04 +0800

    首页列表只显示各类第一集的

--- myLive/readHomeList.php
+++ myLive/readHomeList.php
@@ -1,43 +1,47 @@
 <?php
 /*
-	供首页显示前4个
+	供首页显示前4个，后来改成3个了
 */
 	include "connectMysql.php";
 	set_time_limit(0); //	设置超时时间
 
 	$homeArr4 = array(); 	
 	$newArr = array(	
-		"fileName" => "",
-		"title" => "",
+		"name" => "",
+		"father" => "",
 	);
 
-	$sql1 = mysqli_query($connect,"SELECT * from video WHERE types='电影' ORDER by sort ASC LIMIT 0,4");
+	$sql1 = mysqli_query($connect,"SELECT * from video WHERE types='电影' AND episode=1 ORDER by sort ASC LIMIT 0,4");
 	//	向总数组添加
-	while( $row = mysqli_fetch_assoc($sql1) ){
+	while( $row = mysqli_fetch_assoc($sql1) ){	
+	//	$newArr["id"] = $row["id"];	
+		$newArr["name"] = substr($row["name"],strripos($row["name"],"/")+1);
+		$newArr["father"] = $row["father"];	
+		array_push($homeArr4,$newArr);
+	}
+
+	$sql2 = mysqli_query($connect,"SELECT * from video 	WHERE types='电视剧' AND episode=1 ORDER by sort ASC LIMIT 0,4");
+	//	向总数组添加
+	while( $row = mysqli_fetch_assoc($sql2) ){	
+	//	$newArr["id"] = $row["id"];	
 		$newArr["name"] = substr($row["name"],strripos($row["name"],"/")+1);
 		$newArr["father"] = $row["father"];		
 		array_push($homeArr4,$newArr);
 	}
 
-	$sql2 = mysqli_query($connect,"SELECT * from video 	WHERE types='电视剧' ORDER by sort ASC LIMIT 0,4");
+	$sql3 = mysqli_query($connect,"SELECT * from video 	WHERE types='综艺' AND episode=1 ORDER by sort ASC LIMIT 0,4");
 	//	向总数组添加
-	while( $row = mysqli_fetch_assoc($sql2) ){
+	while( $row = mysqli_fetch_assoc($sql3) ){	
+	//	$newArr["id"] = $row["id"];	
 		$newArr["name"] = substr($row["name"],strripos($row["name"],"/")+1);
 		$newArr["father"] = $row["father"];		
 		array_push($homeArr4,$newArr);
 	}
 
-	$sql3 = mysqli_query($connect,"SELECT * from video 	WHERE types='综艺' ORDER by sort ASC LIMIT 0,4");
+	$sql4 = mysqli_query($connect,"SELECT * from video 	WHERE types='动漫' AND episode=1 ORDER by sort ASC LIMIT 0,4");
 	//	向总数组添加
-	while( $row = mysqli_fetch_assoc($sql3) ){
-		$newArr["name"] = substr($row["name"],strripos($row["name"],"/")+1);
-		$newArr["father"] = $row["father"];		
-		array_push($homeArr4,$newArr);
-	}
-
-	$sql4 = mysqli_query($connect,"SELECT * from video 	WHERE types='动漫' ORDER by sort ASC LIMIT 0,4");
-	//	向总数组添加
-	while( $row = mysqli_fetch_assoc($sql4) ){
+	while( $row = mysqli_fetch_assoc($sql4) ){	
+	//	$newArr["id"] = $row["id"];	
 		$newArr["name"] = substr($row["name"],strripos($row["name"],"/")+1);
 		$newArr["father"] = $row["father"];		
 		array_push($homeArr4,$newArr);
@@ -47,9 +51,6 @@
 
 //		echo "<pre>";
 //		print_r($homeArr4);
-
-
-
 
 
 
